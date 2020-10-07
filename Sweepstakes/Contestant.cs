c830c8ca1fbe50d637222127331c5d922876f15f
Brandon Prange <bprange93@gmail.com>
2020-10-07 13:21:21 -0500

    Comment out the Console.WriteLine line 24-27.

--- Sweepstakes/Contestant.cs
+++ Sweepstakes/Contestant.cs
@@ -6,75 +6,24 @@ using System.Threading.Tasks;
 
 namespace Sweepstakes
 {
-    class Contestant : IUserInterface
+    public class Contestant
     {
-        private string firstName;
-        private string lastName;
-        private string emailAddress;
-        private int registrationNumber;
+        public string FirstName;
+        public string LastName;
+        public string EmailAddress;
+        public int RegistrationNumber;
 
-        public string FirstName
-        {
-            get
-            {
-                return firstName;
-            }
-
-            set
-            {
-                firstName = value;
-            }
-        }
-
-        public string LastName
-        {
-            get
-            {
-                return lastName;
-            }
-
-            set
-            {
-                lastName = value;
-            }
-        }
-
-        public string EmailAddress
-        {
-            get
-            {
-                return emailAddress;
-            }
-
-            set
-            {
-                emailAddress = value;
-            }
-        }
-
-        public int RegistrationNumber
-        {
-            get
-            {
-                return registrationNumber;
-            }
-
-            set
-            {
-                registrationNumber = value;
-            }
-        }
 
         public Contestant(string firstName, string lastName, string emailAddress, int registrationNumber)
         {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.emailAddress = emailAddress;
-            this.registrationNumber = registrationNumber;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.EmailAddress = emailAddress;
+            this.RegistrationNumber = registrationNumber;
         }
-        public void NewContestant(IUserInterface contestant)
-        {
-            Console.WriteLine("Thank you for putting your name in the contest!\n", contestant.FirstName);
-        }
+        //public void NewContestant(IUserInterface contestant)
+        //{
+        //    Console.WriteLine("Thank you for putting your name in the contest!\n", contestant.FirstName);
+        //}
     }
 }
