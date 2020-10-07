a3984140f04aaa7eba24b8ef546ef1770738b481
Brandon Prange <bprange93@gmail.com>
2020-10-07 13:23:54 -0500

    Contestants made in this class. RunMain to run simulation for Sweepstakes.

---
+++ Sweepstakes/Simulation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Simulation
+    {
+        //member variables
+        public Contestant contestant1;
+        public Contestant contestant2;
+        public Sweepstakes sweepstakes1;
+        //constructor
+        public Simulation()
+        {
+            
+        }
+        // member methods
+
+        public void RunMain()
+        {
+            sweepstakes1 = new Sweepstakes("Chachie");                                  //fix later for random number getting deleted. Gets program working right now. 
+            contestant1 = new Contestant("Jack", "Sparrow", "JSparrow@gmail.com", sweepstakes1.GetRandomNumber(50));
+            contestant2 = new Contestant("Steve", "Rogers", "SRogers@gmail.com", sweepstakes1.GetRandomNumber(50));
+            sweepstakes1.RegisterContestant(contestant1);
+            sweepstakes1.RegisterContestant(contestant2);
+            Console.ReadLine();
+        }
+
+        public void CreateMarketingFirmWithManager()
+        {
+
+        }
+    }
+}
