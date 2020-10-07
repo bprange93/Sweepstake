1bb5c90cb59557d89bf7cd8440bea8ffc1db8aad
Brandon Prange <bprange93@gmail.com>
2020-10-07 13:27:30 -0500

    RegisterContestant takes in variables to Add contestant to Dictionary. GetRandomNumber to get the contestant a number before putting them in order.

--- Sweepstakes/Sweepstakes.cs
+++ Sweepstakes/Sweepstakes.cs
@@ -6,25 +6,43 @@ using System.Threading.Tasks;
 
 namespace Sweepstakes
 {
-    public class Sweepstakes : IUserInterface
+    public class Sweepstakes
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string EmailAddress { get; set; }
-        public int RegistrationNumber { get; set; }
+        private Dictionary<int, Contestant> contestants;
+        private string name;
+        public string Name { get; set; }
 
-        public void NewContestant(IUserInterface contestant)
+        public Sweepstakes(string Name)
+        {
+            this.Name = name;
+            contestants = new Dictionary<int, Contestant>();
+        }
+
+                        //move to UserInterface
+        public void RegisterContestant(Contestant contestant) //Assingns contestant obj an int Key and adds the contestants registration
+        {
+            int registerNumber = contestants.Count + 1;
+            contestants.Add(registerNumber, contestant);
+            contestant.RegistrationNumber = registerNumber;
+            Console.WriteLine($"{ contestants.Count} + {contestant.RegistrationNumber} + {contestant.FirstName}");
+        }
+
+        //public Contestant PickWinner()
+        //{
+        //    return;
+        //}
+
+        public void PrintContestantInfo(Contestant contestant)
         {
             
         }
-    }
 
-    //dictonary data structure
-    private List<IUserInterface> contestants = new List<IUserInterface>();
-
-        public void AddContestant(IUserInterface contestants)
+        public int GetRandomNumber(int max)
         {
-        contestants.Add(contestant);
+            Random random = new Random();
+            int randomResult = random.Next(max);
+            return randomResult;
         }
     }
 }
+
